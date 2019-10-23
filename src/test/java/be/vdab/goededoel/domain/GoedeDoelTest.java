82add49006b60d3d11bf860ed2838bf2cfa03dc4
Alina <alina_rodeanu@yahoo.com>
2019-10-23 13:03:50 +0200

    first

---
+++ src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
@@ -0,0 +1,22 @@
+package be.vdab.goededoel.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GoedeDoelTest {
+	private static final String NAAM = "CliniClowns";
+	private GoedeDoel doel;
+
+	@Before
+	public void before() {
+		doel = new GoedeDoel(NAAM);
+	}
+
+	@Test
+	public void getNaam() {
+		assertThat(doel.getNaam()).isEqualTo(NAAM);
+	}
+
+}
